fcf86404dd8b38bd8223d123a0dbbf0150c2b3fd
vimal kumar <vimalkumar@vimals-MacBook-Air.local>
2025-04-05 01:53:22 +0530

    Initial commit

---
+++ Dashboard.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Dashboard = () => {
+    const [feedback, setFeedback] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/feedback')
+            .then(response => setFeedback(response.data))
+            .catch(error => console.error(error));
+    }, []);
+
+    return (
+        <div>
+            <h2>Live Feedback</h2>
+            <ul>
+                {feedback.map((item, index) => (
+                    <li key={index}>{item.text}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Dashboard;
